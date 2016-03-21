e476396c1a7f6bc50b426f20861684a6249ad540
ken(呂冠宏) <ken@edetw.com>
2016-03-21 18:03:17 +0800

    initial commit

---
+++ homework1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(homework1.Startup))]
+namespace homework1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
